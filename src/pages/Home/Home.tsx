92c0d6334e78ecb722f5e9a98cc750d8f43cb808
Kirill <kirillbar24@gmail.com>
2025-02-14 15:15:32 +0400

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+
+import { fetchImageResult } from '@/shared/api/queries.ts';
 import hand from '@/shared/assets/images/hand.png';
 import logo from '@/shared/assets/images/logo.svg';
 import persons from '@/shared/assets/images/persons.png';
@@ -6,6 +10,31 @@ import { BackgroundAnimation } from '@/shared/ui';
 import styles from './Home.module.scss';
 
 export const Home = () => {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get('id');
+
+  const { data: image } = useQuery({
+    queryKey: ['userImage', id],
+    queryFn: () => fetchImageResult(id ? +id : 0),
+    enabled: !!id,
+  });
+
+  const download = () => {
+    if (!image) return;
+
+    fetch(image.image)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const link = document.createElement('a');
+        const url = window.URL.createObjectURL(blob);
+        link.href = url;
+        link.download = 'свадебный_наряд.jpg';
+        link.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((err) => console.error('Ошибка скачивания изображения:', err));
+  };
+
   return (
     <div className={styles.home}>
       <img
@@ -23,16 +52,18 @@ export const Home = () => {
         height={2240}
         className={styles.persons}
       />
-      <div className={styles.hint}>
-        <div className={styles.hintText}>Скачать</div>
-        <img
-          src={hand}
-          alt={''}
-          width={333}
-          height={400}
-          className={styles.hintImage}
-        />
-      </div>
+      {image && (
+        <div className={styles.hint} onClick={download}>
+          <div className={styles.hintText}>Скачать</div>
+          <img
+            src={hand}
+            alt={''}
+            width={333}
+            height={400}
+            className={styles.hintImage}
+          />
+        </div>
+      )}
       <BackgroundAnimation />
     </div>
   );
--- src/shared/consts/index.ts
+++ src/shared/consts/index.ts
@@ -1,4 +1,4 @@
-export const API_URL = window.API_URL || '';
+export const API_URL = window.API_URL || import.meta.env.VITE_API_URL;
 export const DEVICE_ID = window.DEVICE_ID || '';
 export const INACTIVITY_TIMEOUT = window.INACTIVITY_TIMEOUT || 60;
 export const TIMER = window.TIMER || 5;
